985cdfa4e85d1d0e254926f35e8b480dcb4af592
NikollasEdu <nikollas.padilha@escola.pr.gov.br>
2025-10-01 08:50:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function updateClock() {
     let seconds = now.getSeconds();
 
     hours = hours < 10 ? '0' + hours : hours;
-    minutes = minutes < 10 ? 0 + minutes : minutes;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
     const timeString = `${hours}:${minutes}:${seconds}`;
